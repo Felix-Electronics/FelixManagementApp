974646ec68cd8f6f0277885291917bc0efebebba
Aulas <Aulas@DESKTOP-LTFM01M>
2024-11-11 10:30:44 -0600

    Added Cliente class Added conection with SQL Server

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.SqlClient;
+
 namespace FelixManagementApp
 {
     public partial class Form1 : Form
@@ -5,6 +7,23 @@ namespace FelixManagementApp
         public Form1()
         {
             InitializeComponent();
+            ProbarConexion();
+        }
+
+        private void ProbarConexion()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["FelixElectronicsDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open(); MessageBox.Show("Conexión exitosa.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al conectar a la base de datos: " + ex.Message);
+                }
+            }
         }
     }
 }
