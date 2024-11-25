61642f8c9f4bcd1f166e406753fe05e46338e8bd
AldahirM <l21te0275@teziutlan.tecnm.mx>
2024-11-25 10:59:12 -0600

    Added the Orden Frontend

---
+++ Forms/FrmCrearOrden.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace FelixManagementApp.Forms
+{
+    partial class FrmCrearOrden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cbCliente = new ComboBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox5 = new TextBox();
+            comboBox3 = new ComboBox();
+            btnGuardar = new Button();
+            button5 = new Button();
+            lbl = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label11 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // cbCliente
+            // 
+            cbCliente.FormattingEnabled = true;
+            cbCliente.Location = new Point(84, 26);
+            cbCliente.Name = "cbCliente";
+            cbCliente.Size = new Size(194, 23);
+            cbCliente.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(84, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(306, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(482, 285);
+            dataGridView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(203, 55);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(194, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(84, 139);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(194, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(84, 182);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(194, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(84, 225);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(194, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(84, 268);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(194, 96);
+            richTextBox1.TabIndex = 15;
+            richTextBox1.Text = "";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(84, 382);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(194, 23);
+            comboBox2.TabIndex = 18;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(84, 424);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(194, 23);
+            dateTimePicker1.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(84, 467);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(194, 23);
+            textBox5.TabIndex = 21;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(84, 509);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(194, 23);
+            comboBox3.TabIndex = 24;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.BackColor = Color.FromArgb(64, 68, 75);
+            btnGuardar.FlatAppearance.BorderSize = 0;
+            btnGuardar.FlatStyle = FlatStyle.Flat;
+            btnGuardar.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGuardar.ForeColor = Color.White;
+            btnGuardar.Location = new Point(456, 315);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(216, 35);
+            btnGuardar.TabIndex = 27;
+            btnGuardar.Text = "CREAR NUEVA ORDEN";
+            btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(64, 68, 75);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(98, 544);
+            button5.Name = "button5";
+            button5.Size = new Size(164, 23);
+            button5.TabIndex = 28;
+            button5.Text = "AGREGAR COMPUTADORA";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl.ForeColor = Color.White;
+            lbl.Location = new Point(12, 29);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(46, 15);
+            lbl.TabIndex = 29;
+            lbl.Text = "Cliente";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 30;
+            label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 15);
+            label2.TabIndex = 31;
+            label2.Text = "Modelo";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 32;
+            label3.Text = "Contraseña";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(12, 228);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 15);
+            label4.TabIndex = 33;
+            label4.Text = "Accesorios";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(12, 271);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 15);
+            label5.TabIndex = 34;
+            label5.Text = "Problema";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(12, 385);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 15);
+            label6.TabIndex = 35;
+            label6.Text = "Estatus";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(12, 470);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 37;
+            label8.Text = "Costo";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(12, 417);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 15);
+            label7.TabIndex = 39;
+            label7.Text = "Fecha";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(12, 432);
+            label10.Name = "label10";
+            label10.Size = new Size(68, 15);
+            label10.TabIndex = 40;
+            label10.Text = "de entrega";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(4, 517);
+            label9.Name = "label9";
+            label9.Size = new Size(55, 15);
+            label9.TabIndex = 42;
+            label9.Text = "asignado";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(4, 502);
+            label11.Name = "label11";
+            label11.Size = new Size(49, 15);
+            label11.TabIndex = 41;
+            label11.Text = "Técnico";
+            // 
+            // FrmCrearOrden
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(44, 47, 51);
+            ClientSize = new Size(800, 579);
+            Controls.Add(label9);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lbl);
+            Controls.Add(button5);
+            Controls.Add(btnGuardar);
+            Controls.Add(comboBox3);
+            Controls.Add(textBox5);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(cbCliente);
+            Name = "FrmCrearOrden";
+            Text = "FrmCrearOrden";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cbCliente;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox5;
+        private ComboBox comboBox3;
+        private Button btnGuardar;
+        private Button button5;
+        private Label lbl;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label8;
+        private Label label7;
+        private Label label10;
+        private Label label9;
+        private Label label11;
+    }
+}
---
+++ Forms/FrmCrearOrden.cs
@@ -0,0 +1,29 @@
+﻿using FelixManagementApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FelixManagementApp.Forms
+{
+    public partial class FrmCrearOrden : Form
+    {
+        private readonly IClienteService _clienteService;
+        private readonly IEquipoService _equipoService;
+        private readonly IOrdenService _ordenService;
+        private readonly ITecnicoService _tecnicoService;
+        public FrmCrearOrden(IClienteService _clienteService, IEquipoService _equipoService, IOrdenService _ordenService, ITecnicoService _tecnicoService)
+        {
+            InitializeComponent();
+            this._clienteService = _clienteService;
+            this._equipoService = _equipoService;
+            this._ordenService = _ordenService;
+            this._tecnicoService = _tecnicoService;
+        }
+    }
+}
